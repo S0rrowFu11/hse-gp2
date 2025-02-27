2476341d49553c0931a9d89c18f34950726b86af
S0rrowFu11 <dim1tri.1s4ev@gmail.com>
2025-02-28 01:10:02 +0300

    Create new structure of project packages

---
+++ api/__init__.py
@@ -0,0 +1,6 @@
+from .pull_requests_api import get_all_pull_requests
+
+
+__all__ = [
+    "get_all_pull_requests"
+]
--- github_pull_requests_parsing.py
+++ github_pull_requests_parsing.py
@@ -6,19 +6,19 @@ from dotenv import load_dotenv
 
 # название репозитория и время запуска
 repo_name = "ydb-platform_ydb"
-timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S")
 
 # .env
 load_dotenv()
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
-LOG_LEVEL = os.getenv("LOG_LEVEL", "INFO").upper()
+LOGS_LEVEL = os.getenv("LOGS_LEVEL", "INFO").upper()
 
 # настройки логгирования
 log_filename = f"{repo_name}_log_{timestamp}.log"
 json_filename = f"{repo_name}_pull_requests_{timestamp}.json"
 logging.basicConfig(
     filename=log_filename,
-    level=getattr(logging, LOG_LEVEL, logging.INFO),
+    level=getattr(logging, LOGS_LEVEL, logging.INFO),
     format="[%(levelname)s] %(asctime)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
     encoding="utf-8"
@@ -28,7 +28,7 @@ logger = logging.getLogger()
 # для удобства будем еще дублировать в консоль
 console_handler = logging.StreamHandler()
 console_handler.setFormatter(logging.Formatter("[%(levelname)s] %(asctime)s - %(message)s"))
-console_handler.setLevel(getattr(logging, LOG_LEVEL, logging.INFO))
+console_handler.setLevel(getattr(logging, LOGS_LEVEL, logging.INFO))
 logger.addHandler(console_handler)
 
 if not GITHUB_TOKEN:
@@ -36,5 +36,4 @@ if not GITHUB_TOKEN:
     raise ValueError("GITHUB_TOKEN не найден в .env!")
 
 logger.info("Начало парсинга")
-
 
