d5ba7f2af20bd946917a8a2a5e41f25e97454ff8
ariana-ya <pintusik22@mail.ru>
2025-03-01 03:36:57 +0300

    API functions for users

--- api/github_api.py
+++ api/github_api.py
@@ -23,6 +23,7 @@ def get_request(url: str, headers: dict, **kwargs) -> json:
 
 
 def get_all_pull_requests(token: str, owner: str, repo: str) -> list[any]:
+    logging.info(f"Берем все пул реквесты")
     if not token:
         logging.critical('Нет token')
         TypeError('Нет token')
@@ -53,6 +54,7 @@ def get_all_pull_requests(token: str, owner: str, repo: str) -> list[any]:
 
 
 def get_pr_details_data(token: str, owner: str, repo: str, pr_num: int):
+    logging.info(f"Берем информацию о {pr_num} реквесте")
     if not token:
         logging.critical('Нет token')
         TypeError('Нет token')
@@ -70,7 +72,7 @@ def get_pr_details_data(token: str, owner: str, repo: str, pr_num: int):
         'Authorization': f'token {token}',
         'Accept': 'application/vnd.github.v3+json'
     }
-    logging.debug('Запрос на детальную информацию по pr')
+    logging.debug(f'Запрос на детальную информацию по pr {pr_num}')
     return get_request(url=url, headers=header)
 
 
@@ -93,4 +95,39 @@ def get_pr_reviewers(token: str, owner: str, repo: str, pr_num: int):
         'Accept': 'application/vnd.github.v3+json'
     }
     logging.debug(f'Запрос на ревьюеров pr {pr_num}')
+    return get_request(url=url, headers=header)
+
+
+def get_contributors(token: str, owner: str, repo: str):
+    if not token:
+        logging.critical('Нет token')
+        TypeError('Нет token')
+    if not owner:
+        logging.critical('Нет owner')
+        TypeError('Нет owner')
+    if not repo:
+        logging.critical('Нет repo')
+        TypeError('Нет repo')
+    logging.info(f"Берем контрибьюторов {repo}")
+    url = f"https://api.github.com/repos/{owner}/{repo}/contributors"
+    header = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
+    return get_request(url=url, headers=header)
+
+
+def get_user_details(username: str, token: str):
+    if not token:
+        logging.critical('Нет token')
+        TypeError('Нет token')
+    if not username:
+        logging.critical('Нет username')
+        TypeError('Нет username')
+    logging.info(f"Берем детальную информацию о {username}")
+    url = f"https://api.github.com/users/{username}"
+    header = {
+        'Authorization': f'token {token}',
+        'Accept': 'application/vnd.github.v3+json'
+    }
     return get_request(url=url, headers=header)
