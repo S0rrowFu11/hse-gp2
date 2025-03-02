7705e1209a4d010bd0699da56e857264af05675c
daodoevskaya <daodoevskaya@edu.hse.ru>
2025-03-02 16:15:48 +0300

    scrap members

--- scraping_gitlab.py
+++ scraping_gitlab.py
@@ -1,17 +1,39 @@
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-# Настройка опций браузера
+# настройки
 options = webdriver.ChromeOptions()
-# options.add_argument('--headless')
-# options.add_argument('--disable-gpu')
-
+options.add_argument('--headless')
+options.add_argument('--disable-gpu')
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+wait = WebDriverWait(driver, 2)
 
-url = 'https://gitlab.com/gitlab-org/gitlab'
-driver.get(url)
+account_links = []
+page = 1
+base_url = "https://gitlab.com/gitlab-org/gitlab/-/project_members"
 
-time.sleep(1000)
+while True:
+    url = f"{base_url}?page={page}"
+    driver.get(url)
+    print(f"Открываем страницу: {url}")
+    try:
+        # ожидание загрузки
+        wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "a.js-user-link")))
+    except Exception as e:
+        break
+    # берем ссылки на аккаунты
+    members = driver.find_elements(By.CSS_SELECTOR, "a.js-user-link")
+    if not members:
+        print(f"На странице {page} участников не найдено, завершаем цикл.")
+        break
+    for member in members:
+        account_links.append(member.get_attribute("href"))
+    print(f"Страница {page} обработана, всего найдено ссылок: {len(account_links)}")
+    page += 1
+    time.sleep(2)  # задержка, чтобы не уйти в бан
+driver.quit()
